b23a71d55e89c6f914c40e83a9e9d2c022ee562a
Ky <93254944+ikiwz-XD@users.noreply.github.com>
2022-08-04 21:35:59 +0700

    Update yun.py

--- yun.py
+++ yun.py
@@ -1,48 +1,10 @@
-#-----------------------[ SYSTEM-CONTROL ]--------------------#
-if __name__=='__main__':
-	try:os.system('git pull')
-	except:pass
-	try:os.mkdir('OK')
-	except:pass
-	try:os.mkdir('CP')
-	except:pass
-	try:os.mkdir('/sdcard/IKIWZ-DUMP')
-	except:pass
-	try:os.system('touch .prox.txt')
-	except:pass
-	try:os.system('clear')
-	except:pass
-	time.sleep(3)
-	print(f'{k}|||||||||||||||||||||||||||||•••••••••[ WARNING ]•••••••••|||||||||||||||||||||||||||||||')
-	cetak(nel('\t			 SEDANG LOGIN'))
-	print('')
-	time.sleep(5)
-	cetak(nel('\t LOADING..............10%'))
-	time.sleep(8)
-	print(f'{m}<======================================[ {k}20%{x}{m} ]==========================================>')
-	print('')
-	time.sleep(4)
-	print(f'{m}<=======================================[ {k}58%{x}{m} ]=========================================>')
-	print('')
-	time.sleep(6)
-	print(f'{m}<========================================[ {k}65%{x}{m} ]========================================>')
-	print('')
-	time.sleep(8)
-	print(f'{m}<=========================================[ {k}66%{x}{m} ]=======================================>')
-	print('')
-	time.sleep(5)
-	print(f'{m}<==========================================[ {k}76%{x}{m} ]======================================>')
-	time.sleep(5)
-	cetak(nel('\t LOADING..............99%'))
-	time.sleep(8)
-	cetak(nel('\t LOADING..............100%'))
-	print('')
-	time.sleep(10)
-	print('')
-	print('')
-	cetak(nel('\t			LOGIN SUCCESFULLL'))
-	time.sleep(10)
-	login()
+#Hallo anak anjing:v
 
-#     TANK YOU     #
-#     GOOD BYE     #
+import os
+
+if __name__ == "__main__":
+   try:
+       os.system("git pull")
+       __import__("id").moch_ikiwz()
+   except Exception as e:
+       exit(str(e)
