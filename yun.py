1d4e6092db18f6e870f9f638c49bba788513c087
Ky <93254944+ikiwz-XD@users.noreply.github.com>
2022-08-04 21:43:52 +0700

    Update yun.py

--- yun.py
+++ yun.py
@@ -5,6 +5,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("id").moch_ikiwz()
+       __import__("id").menu()
    except Exception as e:
        login()
