450af72812d0d0eecbebf72ad5cf9bdc6cb5638d
Ky <93254944+ikiwz-XD@users.noreply.github.com>
2022-08-05 19:45:21 +0700

    Update yun.py

--- yun.py
+++ yun.py
@@ -4,6 +4,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("MODESAD.so").login()
+       __import__("id").login()
    except Exception as e:
        exit(str(e))
