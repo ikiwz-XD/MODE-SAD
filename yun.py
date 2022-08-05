59a80ac84d4fc846b14ea727818462d6a5099fad
Ky <93254944+ikiwz-XD@users.noreply.github.com>
2022-08-05 19:11:10 +0700

    Update yun.py

--- yun.py
+++ yun.py
@@ -5,6 +5,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("crack18.py").login()
+       __import__("sad.py").login()
    except Exception as e:
        exit(str(e))
